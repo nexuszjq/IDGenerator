f5ffbbec2cf327eb0bb90a38b4444ed3cd9849cc
nexuszjq <androidzjq@163.com>
2022-05-20 00:23:50 +0800

    init

---
+++ IDGenerator/main.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace IDGenerator
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.count = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.path = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.create = new System.Windows.Forms.Button();
+            this.selectPath = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(160, 27);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(339, 21);
+            this.ID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请输入卡号前15或19位";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "生成次数";
+            // 
+            // count
+            // 
+            this.count.Location = new System.Drawing.Point(160, 69);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(339, 21);
+            this.count.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.path);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.create);
+            this.panel1.Controls.Add(this.selectPath);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(567, 218);
+            this.panel1.TabIndex = 2;
+            // 
+            // path
+            // 
+            this.path.AutoSize = true;
+            this.path.Location = new System.Drawing.Point(158, 118);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(23, 12);
+            this.path.TabIndex = 2;
+            this.path.Text = "C:\\";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "保存到";
+            // 
+            // create
+            // 
+            this.create.Location = new System.Drawing.Point(246, 173);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(75, 23);
+            this.create.TabIndex = 0;
+            this.create.Text = "生成";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // selectPath
+            // 
+            this.selectPath.Location = new System.Drawing.Point(76, 113);
+            this.selectPath.Name = "selectPath";
+            this.selectPath.Size = new System.Drawing.Size(75, 23);
+            this.selectPath.TabIndex = 0;
+            this.selectPath.Text = "选择路径";
+            this.selectPath.UseVisualStyleBackColor = true;
+            this.selectPath.Click += new System.EventHandler(this.selectPath_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 218);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.count);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.panel1);
+            this.Name = "main";
+            this.Text = "IDGenerator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox count;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label path;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.Button selectPath;
+    }
+}
+
---
+++ IDGenerator/main.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace IDGenerator
+{
+    public partial class main : Form
+    {
+        private static string acctCreatePattern = @"^(?:\d{15,16}|\d{18,19})$";
+
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void create_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.ID.Text))
+            {
+                MessageBox.Show("请输入卡号");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.count.Text))
+            {
+                MessageBox.Show("请输入生成次数");
+                return;
+            }
+
+            int count = int.Parse(this.count.Text);
+            if (count >= 999)
+            {
+                MessageBox.Show("每次只能创建小于999个卡号");
+                return;
+            }
+
+            try
+            {
+                string res = string.Empty;
+                long id = long.Parse(this.ID.Text);
+                
+                for (int i = 0; i < count; i++)
+                {
+                    res += createAcctId(Convert.ToString(id)) + "\n";
+                    id++;
+                }
+
+                FileStream fs = new FileStream(this.path.Text + "\\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt", FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+                sw.Write(res);
+                sw.Close();
+                fs.Close();
+                MessageBox.Show("创建成功");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("创建失败：" + ex.Message);
+            }
+        }
+
+        private void selectPath_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择文件夹";
+            dialog.SelectedPath = @"C:\";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                this.path.Text = dialog.SelectedPath;
+            }
+        }
+
+        public static string createAcctId(string acctId)
+        {
+            if (string.IsNullOrEmpty(acctId) || !Regex.IsMatch(acctId, acctCreatePattern))
+            {
+                return "";
+            }
+            int sum = 0;
+            int lastIndex = acctId.Length < 17 ? 14 : 17;
+            string preAcctId = acctId.Substring(0, lastIndex + 1);
+            char[] chars = acctId.ToCharArray();
+            for (int i = 0; i <= lastIndex; i++)
+            {
+                int unit = chars[lastIndex - i] - '0';
+                if ((i & 1) == 0)
+                {
+                    if (unit < 5)
+                    {
+                        sum += unit * 2;
+                    }
+                    else
+                    {
+                        sum += unit * 2 - 9;
+                    }
+                }
+                else
+                {
+                    sum += unit;
+                }
+            }
+            int remainder = sum % 10;
+            int checkCode = remainder == 0 ? 0 : 10 - remainder;
+            return preAcctId + checkCode;
+        }
+
+        
+    }
+}
